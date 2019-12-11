4f5d83db44e1e4c794ceec759ed4c7743cbe0ea6
LiamMcIvor <liam.mcivor1@gmail.com>
2019-12-11 17:28:56 +0000

    Core methods added

--- src/main/java/com/bae/user/persistence/domain/User.java
+++ src/main/java/com/bae/user/persistence/domain/User.java
@@ -1,5 +1,210 @@
 package com.bae.user.persistence.domain;
 
-public class User {
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
 
+@Entity
+public class User {
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+	private String firstName;
+	private String surname;
+	private String email;
+	private int houseNumber;
+	private String streetName;
+	private String postcode;
+	private String city;
+	private String country;
+	
+	public User() {}
+
+	
+	public User(Long id, String firstName, String surname, String email, int houseNumber, String streetName,
+			String postcode, String city, String country) {
+		super();
+		this.id = id;
+		this.firstName = firstName;
+		this.surname = surname;
+		this.email = email;
+		this.houseNumber = houseNumber;
+		this.streetName = streetName;
+		this.postcode = postcode;
+		this.city = city;
+		this.country = country;
+	}
+
+	
+
+	public Long getId() {
+		return id;
+	}
+
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+
+	public String getFirstName() {
+		return firstName;
+	}
+
+
+	public void setFirstName(String firstName) {
+		this.firstName = firstName;
+	}
+
+
+	public String getSurname() {
+		return surname;
+	}
+
+
+	public void setSurname(String surname) {
+		this.surname = surname;
+	}
+
+
+	public String getEmail() {
+		return email;
+	}
+
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+
+	public int getHouseNumber() {
+		return houseNumber;
+	}
+
+
+	public void setHouseNumber(int houseNumber) {
+		this.houseNumber = houseNumber;
+	}
+
+
+	public String getStreetName() {
+		return streetName;
+	}
+
+
+	public void setStreetName(String streetName) {
+		this.streetName = streetName;
+	}
+
+
+	public String getPostcode() {
+		return postcode;
+	}
+
+
+	public void setPostcode(String postcode) {
+		this.postcode = postcode;
+	}
+
+
+	public String getCity() {
+		return city;
+	}
+
+
+	public void setCity(String city) {
+		this.city = city;
+	}
+
+
+	public String getCountry() {
+		return country;
+	}
+
+
+	public void setCountry(String country) {
+		this.country = country;
+	}
+
+
+	@Override
+	public String toString() {
+		return "User [id=" + id + ", firstName=" + firstName + ", surname=" + surname + ", email=" + email
+				+ ", houseNumber=" + houseNumber + ", streetName=" + streetName + ", postcode=" + postcode + ", city="
+				+ city + ", country=" + country + "]";
+	}
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((city == null) ? 0 : city.hashCode());
+		result = prime * result + ((country == null) ? 0 : country.hashCode());
+		result = prime * result + ((email == null) ? 0 : email.hashCode());
+		result = prime * result + ((firstName == null) ? 0 : firstName.hashCode());
+		result = prime * result + houseNumber;
+		result = prime * result + ((id == null) ? 0 : id.hashCode());
+		result = prime * result + ((postcode == null) ? 0 : postcode.hashCode());
+		result = prime * result + ((streetName == null) ? 0 : streetName.hashCode());
+		result = prime * result + ((surname == null) ? 0 : surname.hashCode());
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		User other = (User) obj;
+		if (city == null) {
+			if (other.city != null)
+				return false;
+		} else if (!city.equals(other.city))
+			return false;
+		if (country == null) {
+			if (other.country != null)
+				return false;
+		} else if (!country.equals(other.country))
+			return false;
+		if (email == null) {
+			if (other.email != null)
+				return false;
+		} else if (!email.equals(other.email))
+			return false;
+		if (firstName == null) {
+			if (other.firstName != null)
+				return false;
+		} else if (!firstName.equals(other.firstName))
+			return false;
+		if (houseNumber != other.houseNumber)
+			return false;
+		if (id == null) {
+			if (other.id != null)
+				return false;
+		} else if (!id.equals(other.id))
+			return false;
+		if (postcode == null) {
+			if (other.postcode != null)
+				return false;
+		} else if (!postcode.equals(other.postcode))
+			return false;
+		if (streetName == null) {
+			if (other.streetName != null)
+				return false;
+		} else if (!streetName.equals(other.streetName))
+			return false;
+		if (surname == null) {
+			if (other.surname != null)
+				return false;
+		} else if (!surname.equals(other.surname))
+			return false;
+		return true;
+	}
+	
+	
 }
--- src/main/java/com/bae/user/rest/UserController.java
+++ src/main/java/com/bae/user/rest/UserController.java
@@ -1,5 +1,26 @@
 package com.bae.user.rest;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.bae.user.persistence.domain.User;
+import com.bae.user.service.UserService;
+
+@RestController
 public class UserController {
 
+	@Autowired
+	private UserService service;
+	
+	public UserController(UserService service) {
+		this.service = service;
+	}
+	
+	@GetMapping("/createUser")
+	public List<User> getAllUsers() {
+		return service.getAllUsers();
+	}
 }
